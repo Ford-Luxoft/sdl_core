1d0f826f681dc9e8aa813d8721e190dcf28193d2
bsolonenko <bsolonenko@luxoft.com>
2019-01-21 10:29:19 +0200

    answer to review comment

--- tools/infrastructure/run_tests.sh
+++ tools/infrastructure/run_tests.sh
@@ -1,4 +1,34 @@
 #/bin/bash
+# Copyright (c) 2019, Ford Motor Company
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided with the
+# distribution.
+#
+# Neither the name of the Ford Motor Company nor the names of its contributors
+# may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 run_test() {
     full_test_name="$1"
     cur_test="$2"
@@ -28,7 +58,7 @@ run_test() {
     echo "$cur_test_test_count Test #$cur_test: $testname $test_result_text"
 }
 
-run_all_test() {
+run_all_tests() {
     tests=$(find $1 -name '*_test');
     test_count=`echo "$tests" | wc -l`
     cur_test=1
@@ -50,6 +80,6 @@ run_all_test() {
 dir=`pwd`
 
 echo "Test project $dir"
-run_all_test "$dir" "$1"
+run_all_tests "$dir" "$1"
 
 exit $failed_test
