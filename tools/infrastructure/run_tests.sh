64dafdf75dbd9d4ef2008f96320268618357369a
bsolonenko <bsolonenko@luxoft.com>
2019-01-21 14:58:15 +0200

    answer to review comment

--- tools/infrastructure/run_tests.sh
+++ tools/infrastructure/run_tests.sh
@@ -30,45 +30,51 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+# Using:
+# use the -V flag for print the test output if the test fails
+
 run_test() {
     full_test_name="$1"
-    cur_test="$2"
-    test_count="$3"
-    testname=${1##*/}
-    fullpathname=${full_test_name%/*}
+    test_name=${1##*/}
+    full_path_name=${full_test_name%/*}
     cur_dir=$PWD
-    echo "      Start $cur_test: $testname"
 
-    cd $fullpathname
-    test_output=`./$testname 2>&1`
+    cd $full_path_name
+    test_output=`./$test_name 2>&1`
     test_result=`echo $?`
     cd $cur_dir
 
-    testname=`echo $testname | sed -r ':l;/.{40}/q;s/.*/&./;bl'`
-    cur_test_test_count=`echo $cur_test/$test_count | sed -r ':l;/.{5}/q;s/.*/& /;bl'`
+    if [[ $2 == "-V" && $test_result -ne 0 ]]; then
+        echo "\n $test_output \n"
+    fi
+}
+
+print_test_result(){
+    test_name=`echo ${1##*/} | sed -r ':l;/.{40}/q;s/.*/&./;bl'`
+    cur_test_test_count=`echo $2/$3 | sed -r ':l;/.{5}/q;s/.*/& /;bl'`
 
     if [[ $test_result -eq 0 ]]; then
         test_result_text="   Passed"
     else
         test_result_text="***Failed"
-        test_result=1
-        if [[ $4 == "-L" ]]; then
-            echo "\n $test_output \n"
-        fi
     fi
-    echo "$cur_test_test_count Test #$cur_test: $testname $test_result_text"
+
+    echo "$cur_test_test_count Test #$2: $test_name $test_result_text"
 }
 
 run_all_tests() {
-    tests=$(find $1 -name '*_test');
+    echo "Test project $PWD"
+    tests=$(find $PWD -name '*_test')
     test_count=`echo "$tests" | wc -l`
     cur_test=1
     failed_test_count=0
 
     for i in $tests; do
-        run_test "$i" "$cur_test" "$test_count" "$2"
+        echo "      Start $cur_test: $test_name"
+        run_test "$i" "$1"
+        print_test_result "$i" "$cur_test" "$test_count"
         if [[ $test_result -ne 0 ]]; then
-            failed_tests="$failed_tests\n      $testname"
+            failed_tests="$failed_tests\n      $test_name"
             (( failed_test_count+=1 ))
         fi
         (( cur_test+=1 ))
@@ -78,9 +84,6 @@ run_all_tests() {
     echo "$failed_tests \n"
 }
 
-dir=`pwd`
-
-echo "Test project $dir"
-run_all_tests "$dir" "$1"
+run_all_tests "$1"
 
 exit $failed_test_count
